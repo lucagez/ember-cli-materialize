cb96a8d3acff3643cb331495a170ade2e45bd601
lucagez <lucagesmundo@yahoo.it>
2019-10-22 14:20:10 +0100

    remove wrong deprecation meta

--- app/components/materialize-tabs.js
+++ app/components/materialize-tabs.js
@@ -1,11 +1,9 @@
-import { deprecate } from '@ember/application/deprecations';
+
+
 import materializeTabs from './md-tabs';
 
 export default materializeTabs.extend({
   init() {
     this._super(...arguments);
-    deprecate('{{materialize-tabs}} has been deprecated. Please use {{md-tabs}} instead', false, {
-      url: 'https://github.com/sgasser/ember-cli-materialize/issues/67'
-    });
   }
 });
